c43a878c0a2735617c32957b175bd50522f3fe2e
Harshil Darji <darjiharshil2994@gmail.com>
2016-04-28 21:53:07 +0530

    Added two missing examples in 10_Requests

--- 10_Requests/app.js
+++ 10_Requests/app.js
@@ -1,5 +1,21 @@
 var request = require("request");
- 
-request("http://www.bing.com", function(error, response, body) {
-  console.log(body);
-});
+var fs = require("fs");
+
+//Example 1: Grab a Web Page and save it as a text file.
+request("http://www.bing.com", function (error, response, body) { }).pipe(fs.createWriteStream("bing.txt"));
+
+
+//Example 2: Simple download of a file without any callback.
+/*
+request("https://nodejs.org/static/apple-touch-icon.png").pipe(fs.createWriteStream("node.png"));
+*/
+
+
+//Example 3: Simple download of a file and get notified when done.
+/*
+var file = fs.createWriteStream("node.png");
+request("https://nodejs.org/static/apple-touch-icon.png").pipe(file);
+file.on('finish',function(){
+    console.log("Done");
+})
+*/
